ae856108935f76073c662888ef7a1876dbfe307b
munsangu <ds-3632@hanmail.net>
2021-10-10 01:18:29 +0900

    20211010

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import {
   Jumbotron,
 } from "react-bootstrap";
 import Data from "./data";
+import Detail from "./Detail";
 
 import { Link, Route, Switch } from "react-router-dom";
 
@@ -23,8 +24,8 @@ function App() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
+            <Nav.Link><Link to="/">Home</Link></Nav.Link>
+            <Nav.Link><Link to="/detail">Detail</Link></Nav.Link>
             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
@@ -44,46 +45,36 @@ function App() {
         </Navbar.Collapse>
       </Navbar>
 
-      <Route exact path="/">
-        <Jumbotron className="background">
-          <h1>20% Season Off</h1>
-          <p>
-            This is a simple hero unit, a simple jumbotron-style component for
-            calling extra attention to featured content or information.
-          </p>
-          <p>
-            <Button variant="primary">Learn more</Button>
-          </p>
-        </Jumbotron>
+      <Switch>
+        <Route exact path="/">
+          <Jumbotron className="background">
+            <h1>20% Season Off</h1>
+            <p>
+              This is a simple hero unit, a simple jumbotron-style component for
+              calling extra attention to featured content or information.
+            </p>
+            <p>
+              <Button variant="primary">Learn more</Button>
+            </p>
+          </Jumbotron>
 
-        <div className="container">
-          <div className="row">
-            {shoes.map((a, i) => {
-              return <Card shoes={shoes[i]} i={i} key={i} />;
-            })}
-          </div>
-        </div>
-      </Route>
-      <Route path="/detail">
-        <div>
           <div className="container">
             <div className="row">
-              <div className="col-md-6">
-                <img
-                  src="https://codingapple1.github.io/shop/shoes1.jpg"
-                  width="100%"
-                />
-              </div>
-              <div className="col-md-6 mt-4">
-                <h4 className="pt-5">상품명</h4>
-                <p>상품설명</p>
-                <p>120000원</p>
-                <button className="btn btn-danger">주문하기</button>
-              </div>
+              {shoes.map((a, i) => {
+                return <Card shoes={shoes[i]} i={i} key={i} />;
+              })}
             </div>
           </div>
-        </div>
-      </Route>
+        </Route>
+
+        <Route path="/detail">
+            <Detail></Detail>
+        </Route>
+
+        <Route path="/:id">
+              <div>아무거나 적었을 때 보여지는 화면</div>
+        </Route>
+      </Switch>
     </div>
   );
 }
